81ff2a7b85db5a7414fcc0c1b8f58c336269391a
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-24 20:26:47 +0000

    Ex 4.2: refactored project structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
---
+++ utils/logger.js
@@ -0,0 +1,6 @@
+const error = (...params) => console.error(...params)
+const info = (...params) => console.log(...params)
+
+module.exports = {
+  error, info
+}
