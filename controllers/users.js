3a5309d6a406d8451127fbe960ef5712d3265ebb
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-09 09:59:05 +0100

    Ex 4.16: username and password validation

--- controllers/users.js
+++ controllers/users.js
@@ -10,14 +10,27 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   body = request.body
 
-  const passwordHash = await bcrypt.hash(body.password, 10)
+  // Input validation
+  const lengthCorrect = (body.username.length > 3) && (body.password.length > 3)  
+  const users = await User.find({})
+  const usernames = users.map(user => user.username)
+  const uniqueUsername = !usernames.includes(body.username)
 
+  if (!(body.username && body.password && lengthCorrect && uniqueUsername)) {
+    return response
+      .status(401)
+      .json({
+        error: 'invalid username or password'
+      })
+  }
+
+  // Save new user to Db
+  const passwordHash = await bcrypt.hash(body.password, 10)
   const user = new User({
     username: body.username,
     name: body.name,
     passwordHash,
   })
-
   const savedUser = await user.save()
 
   response.json(savedUser)
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -17,6 +17,19 @@ describe('when there is one user in db', () => {
     await user.save()
   })
 
+  test('GET returns the list of users', async () => {
+    const usersInDb = await User.find({})
+    
+    const response = await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    
+    const usernames = response.body.map(user => user.username)
+    expect(usernames).toHaveLength(usersInDb.length)
+    expect(usernames).toContain(usersInDb[0].username)
+  })
+
   test('creation succeeds with a fresh username', async () => {
     const usersAtStart = await helper.usersInDb()
 
@@ -37,19 +50,74 @@ describe('when there is one user in db', () => {
 
     const usernames = usersAtEnd.map(u => u.username)
     expect(usernames).toContain(newUser.username)
+  })  
+})
+
+describe('username and password input validation', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('secret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
   })
 
-  test('GET returns the list of users', async () => {
-    const usersInDb = await User.find({})
-    
-    const response = await api
-      .get('/api/users')
-      .expect(200)
+  test('creation fails when username is too short', async () => {
+    const invalidUser = {
+      username: 'ab',
+      name:'Invalid username',
+      password: '123456',
+    }
+
+    await api
+      .post('/api/users')
+      .send(invalidUser)
+      .expect(401)
       .expect('Content-Type', /application\/json/)
     
-    const usernames = response.body.map(user => user.username)
-    expect(usernames).toHaveLength(usersInDb.length)
-    expect(usernames).toContain(usersInDb[0].username)
+    const usersAtEnd = await helper.usersInDb()
+    const usernames = usersAtEnd.map(u => u.username)
+
+    expect(usernames).not.toContain(invalidUser.username)
+  })
+
+  test('creation fails when password is too short', async () => {
+    const invalidUser = {
+      username: 'abcd',
+      name:'Invalid password',
+      password: '1',
+    }
+
+    await api
+      .post('/api/users')
+      .send(invalidUser)
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+    
+    const usersAtEnd = await helper.usersInDb()
+    const usernames = usersAtEnd.map(u => u.username)
+
+    expect(usernames).not.toContain(invalidUser.username)
+  })
+
+  test('creation fails when username is not unique', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const invalidUser = {
+      username: 'root',
+      name:'Duplicate user',
+      password: '145345',
+    }
+
+    await api
+      .post('/api/users')
+      .send(invalidUser)
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+    
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
