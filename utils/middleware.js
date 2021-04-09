2f35c26aa60972039a4570e68c329c7bea689fb6
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-09 12:51:11 +0100

    4.19: added blog POST requires a valid token

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,14 +1,19 @@
 const logger = require('./logger')
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
   else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message })
   }
+  else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  }
+
+  logger.error(error.message)
 
   next(error)
 }
